a7af349569393a769be2587a8aceb4a31d46eb25
abel <abel.wang@gmail.com>
2019-09-06 16:02:44 -0700

    trying to add execute package from github sdk

--- src/SqlPackage.ts
+++ src/SqlPackage.ts
@@ -1,10 +1,13 @@
 
 import * as core from '@actions/core';
+
     
 export class SqlPackage {
     name: string;
+    exec;
 
     constructor() {
+        this.exec = require('@actions/exec');
         const myInput = core.getInput('myInput');
         this.name = myInput;
     }
@@ -12,6 +15,8 @@ export class SqlPackage {
     execute():void {
         console.log("oookkkkkk " + this.name);
         core.addPath('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\Extensions\\Microsoft\\SQLDB\\DAC\\150');
+
+        this.exec.exec("sqlpackage.exe")
     }
 }
 
