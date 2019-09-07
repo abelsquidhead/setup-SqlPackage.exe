271bbb8c984f9114f1019927f9ed052a01498e61
abel <abel.wang@gmail.com>
2019-09-06 17:30:19 -0700

    added variables and code to call sql package to deploy a dacpac

--- src/SqlPackage.ts
+++ src/SqlPackage.ts
@@ -2,20 +2,27 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
     
 export class SqlPackage {
-    name: string;
+    connectionString: string;
+    dacpac: string;
+    additionalArguments: string;
 
     constructor() {
-        const myInput = core.getInput('myInput');
-        this.name = myInput;
+        this.connectionString= core.getInput('connectionString');
+        this.dacpac = core.getInput('dacpac');
+        this.additionalArguments = core.getInput('additionalArguments');
     }
 
     execute():void {
         // add sql package.exe to path
-        console.log("oookkkkkk " + this.name);
         core.addPath('C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\Common7\\IDE\\Extensions\\Microsoft\\SQLDB\\DAC\\150');
 
         // call sql package.exe
-        exec.exec("sqlpackage.exe")
+        console.log("updating database...");
+        console.log("connectionString: " + this.connectionString);
+        console.log("dacpac: " + this.dacpac);
+        console.log("additionalArguments: " + this.additionalArguments);
+
+        exec.exec("sqlpackage.exe /Action:Publish /SourceFile:\"" + this.dacpac + "\" /TargetConnectionString:\"" + this.connectionString + "\" " + this.additionalArguments);
+        console.log("done updating database");
     }
 }
-
